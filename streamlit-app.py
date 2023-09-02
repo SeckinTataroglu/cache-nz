e3aa4cb6de8f187ce49f7456eea6b9cae64db146
Seckin Tataroglu <seckintataroglu@gmail.com>
2023-09-03 02:44:01 +0300

    month, dayname selection added

--- streamlit-app.py
+++ streamlit-app.py
@@ -43,41 +43,56 @@ def unique_riccardo_class_list():
     conn.close()
     return category_list
 
+def unique_months_list():
+    conn = sqlite3.connect(db_path)
+    query = f"""select distinct "Month" from {table_name}"""
+    df = pd.read_sql(query, con=conn)
+    month_list = df.loc[:, "Month"].tolist()
+    conn.close()
+    return month_list
+
+def unique_dayname_list():
+    conn = sqlite3.connect(db_path)
+    query = f"""select distinct "DayName" from {table_name}"""
+    df = pd.read_sql(query, con=conn)
+    dayname_list = df.loc[:, "DayName"].tolist()
+    conn.close()
+    return dayname_list
+
+
+def selection_to_tuple(selection):
+    if len(selection) == 1:
+        selection_str = str(tuple(selection))[:-2]+")"
+    else:
+        selection_str = str(tuple(selection))
+    return selection_str
+
+
 
 def selected_vessels_df(selected_vessels, 
                         selected_ais_class, 
                         selected_riccardo_class, 
-                        selected_movement):
+                        selected_movement,
+                        selected_month,
+                        selected_dayname
+                        ):
     conn = sqlite3.connect(db_path)
-    # selected vessels
-    if len(selected_vessels) == 1:
-        selected_vessels_str = str(tuple(selected_vessels))[:-2]+")"
-    else:
-        selected_vessels_str = str(tuple(selected_vessels))
-
-    # selected riccardo class
-    if len(selected_ais_class) == 1:
-        selected_ais_class_str = str(tuple(selected_ais_class))[:-2]+")"
-    else:
-        selected_ais_class_str = str(tuple(selected_ais_class))
-
-    # selected riccardo class
-    if len(selected_riccardo_class) == 1:
-        selected_riccardo_class_str = str(tuple(selected_riccardo_class))[:-2]+")"
-    else:
-        selected_riccardo_class_str = str(tuple(selected_riccardo_class))
     
-    # selected movements
-    if len(selected_movement) == 1:
-        selected_movement_str = str(tuple(selected_movement))[:-2]+")"
-    else:
-        selected_movement_str = str(tuple(selected_movement))
+    # selections
+    selected_vessels_str = selection_to_tuple(selected_vessels)
+    selected_ais_class_str = selection_to_tuple(selected_ais_class)
+    selected_riccardo_class_str = selection_to_tuple(selected_riccardo_class)
+    selected_movement_str = selection_to_tuple(selected_movement)
+    selected_month_str = selection_to_tuple(selected_month)
+    selected_dayname_str = selection_to_tuple(selected_dayname)
     
     query = f"""select * from {table_name} 
                 where "Vessel Name" in {selected_vessels_str} 
                 and "TYPE_NAME" in {selected_ais_class_str}
                 and "Riccardo Class" in {selected_riccardo_class_str}
                 and "Movement_Type" in {selected_movement_str}
+                and "Month" in {selected_month_str}
+                and "DayName" in {selected_dayname_str}
                 limit 10000"""
     df = pd.read_sql(query, con=conn)
     conn.close()
@@ -100,19 +115,19 @@ with st.sidebar:
 
     # Vessels List
     unique_vessels = unique_vessels_list()
-    selected_vessels = st.multiselect("Select Vessels:",["All"] + ["---"] + unique_vessels, default=["All"])
+    selected_vessels = st.multiselect("Select Vessels:",["All", "---"] + unique_vessels, default=["All"])
     if "All" in selected_vessels:
         selected_vessels = unique_vessels
 
     # AIS Class
     unique_ais_class = unique_ais_class_list()
-    selected_ais_class = st.multiselect("Select AIS Class (TYPE_NAME):",["All"] + ["---"] + unique_ais_class, default=["All"])
+    selected_ais_class = st.multiselect("Select AIS Class (TYPE_NAME):",["All", "---"] + unique_ais_class, default=["All"])
     if "All" in selected_ais_class:
         selected_ais_class = unique_ais_class
     
     # Riccardo Class
     unique_riccardo_class = unique_riccardo_class_list()
-    selected_riccardo_class = st.multiselect("Select Riccardo Class:",["All"] + ["---"] + unique_riccardo_class, default=["All"])
+    selected_riccardo_class = st.multiselect("Select Riccardo Class:",["All", "---"] + unique_riccardo_class, default=["All"])
     if "All" in selected_riccardo_class:
         selected_riccardo_class = unique_riccardo_class
 
@@ -120,6 +135,18 @@ with st.sidebar:
     selected_movement = st.multiselect("Select Movement:",["All", "---", "Inbound", "Berth", "Outbound"], default=["All"])
     if "All" in selected_movement:
         selected_movement = ["Inbound", "Berth", "Outbound"]
+
+    # Month
+    unique_months = unique_months_list()
+    selected_month = st.multiselect("Select Months:",["All", "---",] + unique_months, default=["All"])
+    if "All" in selected_month:
+        selected_month = unique_months
+
+    # Day of Week
+    unique_dayname = unique_dayname_list()
+    selected_dayname = st.multiselect("Select Days of Week:",["All", "---",] + unique_dayname, default=["All"])
+    if "All" in selected_dayname:
+        selected_dayname = unique_dayname
 
     
     
@@ -133,7 +160,9 @@ if fetch_button:
     selected_vessels_df_ = selected_vessels_df(selected_vessels=selected_vessels,
                                                selected_ais_class=selected_ais_class,
                                                selected_riccardo_class=selected_riccardo_class,
-                                               selected_movement=selected_movement)
+                                               selected_movement=selected_movement,
+                                               selected_month=selected_month,
+                                               selected_dayname=selected_dayname)
     st.write(selected_vessels_df_)
 
 
